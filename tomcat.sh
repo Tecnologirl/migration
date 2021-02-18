4b0114ea44ab7f6addcdcbe8d5a6183f82eb2b33
Alondra Mejía <alondra@technologirl.dev>
2021-02-18 08:55:25 -0600

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -31,7 +31,7 @@ sudo cp tomcat-users.xml /usr/share/tomcat/conf/tomcat-users.xml
 sudo mkdir /usr/share/tomcat/conf/sslkey
 sudo cp sslkey/webserverkey /usr/share/tomcat/conf/sslkey/webserverkey
 #agregamos el archivo con la dirección del ssl actualizada
-sudo cp server.xml /usr/share/tomcat/conf/server.xml
+#sudo cp server.xml /usr/share/tomcat/conf/server.xml
 
 #Probando el servidor
 sudo systemctl daemon-reload
