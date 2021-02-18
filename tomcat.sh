93bc8d206b0da434a6722c946529198feb4b9f33
Alondra Mejía <alondra@technologirl.dev>
2021-02-18 00:22:57 -0600

    agregando ssl y usuarios al script

--- tomcat.sh
+++ tomcat.sh
@@ -6,42 +6,50 @@
 #chmod +x tomcat.sh
 #./tomcat.sh
 
-#Se crea el grupo de usuarios 
+#Se crea el grupo de usuarios
 
-sudo groupadd --system tomcat 
-sudo useradd -d /usr/share/tomcat -r -s /bin/false -g tomcat tomcat 
+sudo groupadd --system tomcat
+sudo useradd -d /usr/share/tomcat -r -s /bin/false -g tomcat tomcat
 
 #instalación de java
 sudo yum -y install java-1.8.0-openjdk-devel
 
 #instalación tomcat
-sudo yum -y install wget 
-export VER="9.0.30" 
-wget https://archive.apache.org/dist/tomcat/tomcat-9/v${VER}/bin/apache-tomcat-${VER}.tar.gz 
-sudo tar xvf apache-tomcat-${VER}.tar.gz -C /usr/share/ 
-sudo ln -s /usr/share/apache-tomcat-$VER/ /usr/share/tomcat 
-sudo chown -R tomcat:tomcat /usr/share/tomcat 
-sudo chown -R tomcat:tomcat /usr/share/apache-tomcat-$VER/  
+sudo yum -y install wget
+export VER="9.0.30"
+wget https://archive.apache.org/dist/tomcat/tomcat-9/v${VER}/bin/apache-tomcat-${VER}.tar.gz
+sudo tar xvf apache-tomcat-${VER}.tar.gz -C /usr/share/
+sudo ln -s /usr/share/apache-tomcat-$VER/ /usr/share/tomcat
+sudo chown -R tomcat:tomcat /usr/share/tomcat
+sudo chown -R tomcat:tomcat /usr/share/apache-tomcat-$VER/
 
-#Archivo .service para ejecutar los comandos de Tomcat (start, stop, etc) 
-#sudo vim /etc/systemd/system/tomcat.service 
-sudo cp ./tomcat.service /etc/systemd/system/tomcat.service 
+#Archivo .service para ejecutar los comandos de Tomcat (start, stop, etc)
+#sudo vim /etc/systemd/system/tomcat.service
+sudo cp ./tomcat.service /etc/systemd/system/tomcat.service
+#agregamos el archivo con los usuarios
+sudo cp ./tomcat-users.xml /usr/share/tomcat/conf/tomcat-users.xml
+#agregamos el certificado ssl
+sudo mkdir /usr/share/tomcat/conf/sslkey
+sudo cp ./sslkey/webserverkey
+#agregamos el archivo con la dirección del ssl actualizada
+sudo cp ./server.xml /usr/share/tomcat/conf/server.xml
 
-#Probando el servidor 
-sudo systemctl daemon-reload 
-sudo systemctl start tomcat 
-sudo systemctl enable tomcat 
-#systemctl status tomcat 
+#Probando el servidor
+sudo systemctl daemon-reload
+sudo systemctl start tomcat
+sudo systemctl enable tomcat
+#systemctl status tomcat
 
-#Firewall 
-sudo firewall-cmd --permanent --add-port=8080/tcp 
-sudo firewall-cmd –reload 
+#Firewall
+sudo firewall-cmd --permanent --add-port=8080/tcp
+sudo firewall-cmd --permanent --add-port=8443/tcp
+sudo firewall-cmd –reload
 
-#Agregando usuarios 
-#sudo vi /usr/share/tomcat/conf/tomcat-users.xml 
+#Agregando usuarios
+#sudo vi /usr/share/tomcat/conf/tomcat-users.xml
 
 #revisar lo del proxy
-sudo yum -y install httpd 
+sudo yum -y install httpd
 #/etc/httpd/conf.d/tomcat_manager.conf
 sudo cp ./tomcat_manager.conf /etc/httpd/conf.d/tomcat_manager.conf
 
